9a9ca827a9ba7de96d90403266e0d5e96136080d
Dinesh Khairnar <96805747+dskhairnar@users.noreply.github.com>
2026-06-08 13:40:41 +0530

    fix: Implement AppToaster component for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from 'react-router';
 import { MainRouter } from '@/core/routes/Routes';
 import { UserProvider } from '@/hooks/UserContext';
-import { Toaster } from 'react-hot-toast';
 import { DocsProvider } from './context/DocsContext';
+import AppToaster from './components/atoms/AppToaster/AppToaster';
 import ReactQueryProvider from './core/services/tanstack/ReactQueryProvider';
 import useVersionCheck from '@/hooks/useVersionCheck';
 import { PaddleProvider } from '@/core/paddle';
@@ -18,35 +18,7 @@ const App = () => {
 						<RouterProvider router={MainRouter} />
 					</DocsProvider>
 
-					{/* Toast Notifications */}
-					<Toaster
-						toastOptions={{
-							duration: 4000,
-							style: {
-								maxWidth: 'min(calc(100vw - 32px), 520px)',
-								wordBreak: 'break-word',
-								overflowWrap: 'break-word',
-							},
-							success: {
-								iconTheme: {
-									primary: '#5CA7A0',
-									secondary: '#fff',
-								},
-								className: 'whitespace-nowrap',
-							},
-							error: {
-								iconTheme: {
-									primary: '#E76E50',
-									secondary: '#fff',
-								},
-								className: 'break-words',
-							},
-						}}
-						position='bottom-center'
-						containerStyle={{
-							bottom: '80px',
-						}}
-					/>
+					<AppToaster />
 					<div id='modal-root'></div>
 				</PaddleProvider>
 			</UserProvider>
