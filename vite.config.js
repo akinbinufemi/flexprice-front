e5a697169a62b09166bfcebec50f961e6a09a814
akinbinufemi <akinbinufemi.s@gmail.com>
2026-06-17 15:31:32 +0300

    Update Vite config for server CORS and preview settings

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import { defineConfig } from 'vite';
+
 var meta = JSON.parse(fs.readFileSync('./public/meta.json', 'utf8'));
+
 export default defineConfig({
     plugins: [react()],
     define: {
@@ -13,12 +15,18 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+    // This handles 'npm run dev' mode
     server: {
         cors: {
-            origin: 'http://localhost:3000',
+            origin: '*',
             methods: ['GET', 'POST'],
         },
         host: '0.0.0.0',
-        allowedHosts: true,
+        allowedHosts: ['billing.izini.ng'],
     },
+    // This handles 'npm run preview' mode (What Dokploy is likely using)
+    preview: {
+        host: '0.0.0.0',
+        allowedHosts: ['billing.izini.ng'],
+    }
 });
